9f2a79cd0fc9a2712ce37674a6ee0278b829049a
MadeAditya02 <imadeaditya4@gmail.com>
2024-11-20 21:37:55 +0800

    Tambah routing

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
 
-Route::get('/', [MainController::class, 'index']);
-Route::get('/about', [MainController::class, 'index']);
-Route::get('/products', [MainController::class, 'index']);
+Route::get('/cart', [MainController::class, 'index']);
+Route::get('/cart/checkout', [MainController::class, 'index']);
+Route::get('/login', [MainController::class, 'index']);
+Route::get('/logout', [MainController::class, 'index']);
+Route::get('/register', [MainController::class, 'index']);
+Route::get('/admin', [MainController::class, 'index']);

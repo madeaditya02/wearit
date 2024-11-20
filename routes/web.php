168c5b96162d9845712df696ffc91a4b0f91a0c4
MadeAditya02 <imadeaditya4@gmail.com>
2024-11-20 21:35:52 +0800

    Edit routing

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
 
-Route::get('/', [MainController::class, 'index']);
-Route::get('/about', [MainController::class, 'index']);
+Route::get('/test', [MainController::class, 'index']);
+Route::get('/test1', [MainController::class, 'index']);
 Route::get('/products', [MainController::class, 'index']);
+Route::get('/products/detail', [MainController::class, 'index']);
+Route::get('/products/checkout', [MainController::class, 'index']);

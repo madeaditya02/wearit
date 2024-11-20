5f4e8afae60365d22199d4b34b1f5a7890d00ddf
MadeAditya02 <imadeaditya4@gmail.com>
2024-11-20 15:40:29 +0800

    Add Layout Navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,26 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
+import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.vue',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
     ],
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+                poppins: ["Poppins", "sans-serif"],
+            },
+            colors: {
+                primary: "#002E89",
+                "primary-darker": "#012B89",
+                "primary-dark": "#001246",
+                "primary-light": "#3751FE",
+                "gray-light": "#E9EEF9",
             },
         },
     },

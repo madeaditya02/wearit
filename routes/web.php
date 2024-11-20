f790d0da05c1c1610dfe499767c806e1104fc532
MadeAditya02 <imadeaditya4@gmail.com>
2024-11-20 21:20:42 +0800

    Edit route web

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
 
 Route::get('/', [MainController::class, 'index']);
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/about', [MainController::class, 'index']);

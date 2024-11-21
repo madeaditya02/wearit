3ee0a4fef0b40c3fc397350949fcf8418a926063
riyaniastarani6767 <riyaniastarani822@gmail.com>
2024-11-21 15:45:01 +0800

    pesan

---
+++ resources/views/product/ProductDisplay.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.user')
+
+@section('title')
+Products
+@endsection
+@section('content')
+<div>
+    <h1>Hallo Dunia Apa Kabar</h1>
+    <h2>hhhhahshhahhshd</h2>
+</div>
+@endsection

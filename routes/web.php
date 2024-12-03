e6a19acf5a17eaa8692490cc7ca5d872b4f84dfe
satriadev <satriahgm@gmail.com>
2024-12-03 12:39:13 +0800

    Pesan commit

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\ProductController;
-<<<<<<< HEAD
+
 use App\Http\Controllers\DetailProductController;
 
 Route::get('/', function (){
@@ -11,7 +11,3 @@ Route::get('/', function (){
 });
 
 Route::get('/detailproduct', [DetailProductController::class, 'DetailProduct']);
-=======
-
-Route::get('/', [MainController::class, 'index']);
->>>>>>> 5128264ef8a07be763f1ababbc90675e219bf864
